0046c0b384c53bde9e31abed1fc0a816a6ffad60
adarsh02 <aadarshraazz007@gmail.com>
2024-12-03 23:13:06 +0530

    updated navbar dynamically

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,48 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserDataContext } from "../context/UserContext";
 
 const Navbar = () => {
+  const { user, setUser } = useContext(UserDataContext);
+  const navigate = useNavigate();
+
+  const logoutHandler = () => {
+    setUser({
+      email: "",
+      fullname: {
+        firstName: "",
+        lastName: "",
+      },
+    });
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
   return (
     <div className="bg-zinc-900 px-5 sm:px-10 py-3 flex items-center justify-between">
       <div>
         <h2 className="text-white font-semibold text-2xl">Test</h2>
       </div>
       <div className="flex gap-10">
-        <Link to="/login" className="text-white">
-          Login
-        </Link>
-        <Link to="/register" className="text-white">
-          Register
-        </Link>
+        {!user.email ? (
+          <>
+            <Link to="/login" className="text-white">
+              Login
+            </Link>
+            <Link to="/register" className="text-white">
+              Register
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link to="/create-test" className="text-white">
+              {user.fullname.firstName} {user.fullname.lastName}
+            </Link>
+            <button onClick={logoutHandler} className="text-white">
+              Logout
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
